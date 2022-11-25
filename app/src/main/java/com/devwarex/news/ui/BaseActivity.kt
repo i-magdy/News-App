cbaf1edb01b70e660b637031f0a8bd157aaa73c3
Ibrahim Magdy <ibrahim.magdy31@gmail.com>
2022-11-25 15:08:44 +0200

    enable loclaize app language

--- app/src/main/java/com/devwarex/news/ui/BaseActivity.kt
+++ app/src/main/java/com/devwarex/news/ui/BaseActivity.kt
@@ -1,6 +1,17 @@
 package com.devwarex.news.ui
 
+import android.app.LocaleManager
+import android.os.Build
+import android.os.Bundle
+import android.os.LocaleList
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.os.LocaleListCompat
+import androidx.lifecycle.lifecycleScope
+import com.devwarex.news.data.DatastoreImpl
+import java.util.*
+import javax.inject.Inject
 
 open class BaseActivity : AppCompatActivity {
 
@@ -9,6 +20,34 @@ open class BaseActivity : AppCompatActivity {
         contentLayoutId: Int
     ):super(contentLayoutId)
 
+    @Inject
+    lateinit var datastoreImpl: DatastoreImpl
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (this::datastoreImpl.isInitialized) {
+            lifecycleScope.launchWhenCreated {
+                datastoreImpl.appLanguage.collect {
+                    val lang = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N){
+                        resources.configuration.locales[0].language
+                    }else{
+                        resources.configuration.locale.language
+                    }
+                    if (lang != it){
+                        changeAppLanguage(it)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun changeAppLanguage(lang: String){
+        val appLocale = LocaleListCompat.forLanguageTags(lang)
+        AppCompatDelegate.setApplicationLocales(appLocale)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            getSystemService(LocaleManager::class.java).applicationLocales = LocaleList(Locale.forLanguageTag(lang))
+        }
+        recreate()
+    }
 
 }
