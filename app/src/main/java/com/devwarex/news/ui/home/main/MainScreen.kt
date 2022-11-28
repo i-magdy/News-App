4f12e60d102146b86a564bfcb7bca71151718f7f
Ibrahim Magdy <ibrahim.magdy31@gmail.com>
2022-11-28 15:30:57 +0200

    Add Swipe refresh in main screen

--- app/src/main/java/com/devwarex/news/ui/home/main/MainScreen.kt
+++ app/src/main/java/com/devwarex/news/ui/home/main/MainScreen.kt
@@ -1,5 +1,6 @@
 package com.devwarex.news.ui.home.main
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
@@ -11,9 +12,11 @@ import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.devwarex.news.R
 import com.devwarex.news.adapters.ArticlesAdapter
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
@@ -24,7 +27,9 @@ class MainScreen : Fragment(
     private val viewModel by viewModels<MainViewModel>()
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val recyclerView = view.findViewById<RecyclerView>(R.id.articles_rv)
+        val swipe = view.findViewById<SwipeRefreshLayout>(R.id.main_swipe)
+        swipe.setColorSchemeResources(R.color.blue_400)
+        swipe.isRefreshing = true
         view.findViewById<ImageView>(R.id.main_search_iv).setOnClickListener {
             Navigation.findNavController(view).navigate(R.id.action_navigate_to_search)
         }
@@ -32,13 +37,24 @@ class MainScreen : Fragment(
             Navigation.findNavController(view).navigate(R.id.action_navigate_to_settings)
         }
         val adapter = ArticlesAdapter(this)
-        recyclerView.adapter = adapter
-        recyclerView.layoutManager = LinearLayoutManager(context)
+        view.findViewById<RecyclerView>(R.id.articles_rv).apply {
+            this.adapter = adapter
+            this.layoutManager = LinearLayoutManager(context)
+        }
+        swipe.setOnRefreshListener {
+            viewModel.refresh()
+            lifecycleScope.launch {
+                delay(700)
+                swipe.isRefreshing = false
+            }
+        }
+
         lifecycleScope.launch {
             viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.CREATED){
                 launch {
                     viewModel.articles.collect{
                         adapter.setArticles(it)
+                        swipe.isRefreshing = false
                     }
                 }
                 launch {
--- app/src/main/java/com/devwarex/news/ui/home/main/MainViewModel.kt
+++ app/src/main/java/com/devwarex/news/ui/home/main/MainViewModel.kt
@@ -6,9 +6,11 @@ import com.devwarex.news.data.DatastoreImpl
 import com.devwarex.news.db.AppDao
 import com.devwarex.news.db.ArticleRelation
 import com.devwarex.news.repos.RefreshArticlesRepo
+import com.devwarex.news.util.TimeoutUtil
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
+import java.util.Calendar
 import javax.inject.Inject
 
 @HiltViewModel
@@ -20,10 +22,12 @@ class MainViewModel @Inject constructor(
 
     private val _articles = MutableStateFlow<List<ArticleRelation>>(emptyList())
     val articles: StateFlow<List<ArticleRelation>> = _articles
+    private var code = ""
 
     init {
         viewModelScope.launch {
             datastore.selectedCountry.collect{ country ->
+                code = country
                 db.getArticlesByCountry(
                     code = country
                 ).collect{ if (it.isNotEmpty()) _articles.emit(it) else  refreshArticlesRepo.sync(country) }
@@ -37,5 +41,15 @@ class MainViewModel @Inject constructor(
         isBooked: Boolean
     ) = viewModelScope.launch {
         db.updateArticle(url = url, isBooked = isBooked)
+    }
+
+    fun refresh() = viewModelScope.launch {
+        if (code.isNotEmpty()){
+            datastore.refreshTimeout.collect{ time ->
+                if (TimeoutUtil.isTimeout(
+                        currentTime = Calendar.getInstance().timeInMillis,
+                        savedTime = time)){ refreshArticlesRepo.sync(code) }
+            }
+        }
     }
 }
